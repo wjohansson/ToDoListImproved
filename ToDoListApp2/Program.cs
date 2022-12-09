80195c204575b7d274074da1d1fbadf9dae5406a
wjohansson <william.johansson1999@outlook.com>
2022-12-09 08:29:25 +0100

    improved structure

--- ToDoListApp2/Program.cs
+++ ToDoListApp2/Program.cs
@@ -4,11 +4,19 @@
     {
         static void Main(string[] args)
         {
-            // Kolla ifall ITaskManager kan användas någonstans
+            // Lägga till möjlighet att gå till förra sidan från alla funktioner, eller åtminstone de
+            //  som skapar och ändrar, alltså en quit/return från överallt
             // Kolla edit funktion ifall den fungerar även fast man använder den från toggelarchive funktionen,
             //  den fungerar inte, måste fixas
             // Kolla are you sure meddelanden, kommer på fel ställen om man kallar dom från togglearchive funktionerna, blir
             //  nog rätt då man fixar edit funktionen
+            // Fixa undo på alla saker, hela vägen tillbaka till ursprung
+
+
+            // Sista koll:
+            // Notera alla funktioner som en kommentar här i program för att komma ihåg allt som går att göra
+            // GÅ igenom hela programmet, och försöka nå alla endpoints med debugger för att kolla så att allt fungerar
+
 
             var activeManager = new ActiveManager();
 
